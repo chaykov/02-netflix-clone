c5ada4ef20da2abb1a86783b96deab893481b610
Chaykov <toumarenshi@gmail.com>
2024-03-16 19:06:14 +0100

    main

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,45 @@
+import Navbar from "@/components/Navbar";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
+  const { data: user, data: account } = useCurrentUser();
+
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+      <Navbar />
+      <div className="bg-gray-500">
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+      </div>
     </>
   );
 }
